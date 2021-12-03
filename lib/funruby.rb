30a7735b2190dd28c1513aede93680b19d91e0e3
jennyshih <jinghua.shih@gmail.com>
2021-12-03 09:29:37 +0800

    Initial setup

---
+++ lib/funruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "funruby/version"
+
+module Funruby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/funruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Funruby
+  VERSION = "0.1.0"
+end
